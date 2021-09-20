a3a9a3de89262587bcfecfd58f9ef9686ff8f178
SihyunSeo <63051137+SihyunSeo@users.noreply.github.com>
2021-09-20 18:46:19 +0900

    chatRoomList

--- shareParking-AndroidStudio/app/src/main/java/com/example/parking/ui/chat/ChatFragment.java
+++ shareParking-AndroidStudio/app/src/main/java/com/example/parking/ui/chat/ChatFragment.java
@@ -27,6 +27,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,33 +35,46 @@ import java.util.List;
 public class ChatFragment extends Fragment {
     private DatabaseReference chattingList;
     private ChatViewModel chatViewModel;
-    private List<String> chatList;
+    private ArrayList<String> list2;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         chatViewModel =
                 ViewModelProviders.of(this).get(ChatViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_chat, container, false);
+        final View root = inflater.inflate(R.layout.fragment_chat, container, false);
+        final ListView list_view = (ListView) root.findViewById(R.id.list2);
 
-        ListView list_view = root.findViewById(R.id.list2);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getContext(),
+                android.R.layout.simple_list_item_1,
+                list2
+        );
 
-        chatList = new ArrayList<>();// 채팅방 목록 표현해주기 위한 리스
 
+        list2 = new ArrayList<String>();// 채팅방 목록 표현해주기 위한 리스트
         FirebaseDatabase database = FirebaseDatabase.getInstance(); //데이터베이스 생성 및 선
-        chattingList = database.getReference("chattingList"); //chattingList 값 가져오기
+        chattingList = database.getReference("chattingList"); //chattingList 값 가져오
+        System.out.println("toString: "+chattingList.toString());
 
-
-//        System.out.println(chattingList.getRef());
         chattingList.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 Log.d("ChattingList", "list : " + dataSnapshot.getValue());
-                chatList.add(dataSnapshot.getValue().toString());
-                System.out.println("--------------------------------채팅방리스트----------------------------------");
-                for(int i = 0; i < chatList.size(); i ++) {
-                    System.out.println(chatList.get(i));
-                }
-                System.out.println("--------------------------------채팅방리스트----------------------------------");
+                list2.add(dataSnapshot.getValue().toString());
+                System.out.println("size:"+ list2.size());
+                System.out.println(s);
+                System.out.println("--------------------------------채팅방리스트(in the void method)----------------------------------");
+//                for(int i = 0; i < list2.size(); i ++) {
+//                    System.out.println(list2.get(i));
+//                }
+
+                list_view.setAdapter(new ArrayAdapter<String>(
+                        getContext(),
+                        android.R.layout.simple_list_item_1,
+                        list2
+                ));
+
+                System.out.println("-----------------------------------------------------------------------------------------------");
             }
 
             @Override
@@ -82,7 +96,17 @@ public class ChatFragment extends Fragment {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
+
         });
+
+        System.out.println("--------------------------------채팅방리스트(out of void method)----------------------------------");
+        for(int i = 0; i < list2.size(); i ++) {
+            System.out.println(list2.get(i));
+        }
+        System.out.println("-----------------------------------------------------------------------------------------------");
+
+//        list_view.setAdapter(adapter);
+
 //        final TextView textView = root.findViewById(R.id.text_chat);
 //        Button button = root.findViewById(R.id.chatting_test);
 //
