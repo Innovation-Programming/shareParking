049df35a269aa531a54aded687309364e5aee296
SihyunSeo <63051137+SihyunSeo@users.noreply.github.com>
2021-09-26 03:40:20 +0900

    edit enterChatRoom

--- shareParking-AndroidStudio/app/src/main/java/com/example/parking/ChatActivity.java
+++ shareParking-AndroidStudio/app/src/main/java/com/example/parking/ChatActivity.java
@@ -34,8 +34,10 @@ public class ChatActivity extends AppCompatActivity {
     public RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private List<ChatData> chatList;
+    private ArrayList<String> Repetition, finalRepetition;
 //    private String nick = "nick2"; //1:1채팅 or 1:다 채팅 ----> nick2 이걸 username으로 쓰고싶으면 인자를 다시 받아와야되는부분?!
     private String rpUser, parkingAdmin;
+    public String reqUser;
     private String chatRoomName;
     private EditText EditText_chat;
     private Button Button_send;
@@ -49,15 +51,23 @@ public class ChatActivity extends AppCompatActivity {
         Button_send = findViewById(R.id.Button_send);
         EditText_chat = findViewById(R.id.EditText_chat);
 
+        //login한 유저값, parkingAdmin 유저값 불러오기
+
         SharedPreferences sharedPreferences = getSharedPreferences("sFile",MODE_PRIVATE);
+        reqUser = sharedPreferences.getString("loginId", "user");
         rpUser = sharedPreferences.getString("loginId", "user");//로그인할때 받아온 접속한 유저의 이름 셋
         parkingAdmin = sharedPreferences.getString("parkingAdmin", "user");//클릭한 주차장 주인 유저의 이름
-        chatRoomName = rpUser + "&" +parkingAdmin;
-        System.out.println("------------------------------------------------------------------");
-        System.out.println(rpUser);
-        System.out.println(parkingAdmin);
-        System.out.println(chatRoomName);
-        System.out.println("------------------------------------------------------------------");
+
+
+//        chatRoomName = rpUser + "&" +parkingAdmin;
+//        System.out.println("------------------------------------------------------------------");
+//        System.out.println(rpUser);
+//        System.out.println(parkingAdmin);
+//        System.out.println(chatRoomName);
+//        System.out.println("------------------------------------------------------------------");
+
+        //채팅 전송 버튼 이벤트리스너
+
         Button_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,10 +78,6 @@ public class ChatActivity extends AppCompatActivity {
                     chat.setNickname(rpUser);
                     chat.setMsg(msg);
                     myRef.push().setValue(chat);
-//                    ChatData chatRoom = new ChatData();
-//                    chatRoom.setUsername(rpUser);
-//                    chatRoom.setParkingAdmin(parkingAdmin);
-//                    myRef.push().setValue(chatRoom);
                     EditText_chat.setText("");
                 }
             }
@@ -81,17 +87,47 @@ public class ChatActivity extends AppCompatActivity {
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
 
+        Repetition = new ArrayList<String>();
+        finalRepetition  = new ArrayList<String>();
         chatList = new ArrayList<>();
         mAdapter = new ChatAdapter(chatList, ChatActivity.this, rpUser);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         mRecyclerView.setAdapter(mAdapter);
         // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance(); // 데이터베이스 생성 및 선언
+        final FirebaseDatabase database = FirebaseDatabase.getInstance(); // 데이터베이스 생성 및 선언
+
+        final String testChatRoom = sharedPreferences.getString("roomName", "user");
+        System.out.println("제대로들어갔니?: " + testChatRoom);
+
         chattingList = database.getReference("chattingList"); //채팅 리스트 database
-        chattingList.push().setValue(chatRoomName);
+
+        chattingList.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                    finalRepetition.add(postSnapshot.getValue().toString());
+                }
+
+                System.out.println("호옹익?:" + finalRepetition);
+                System.out.println("호옹익?:" + testChatRoom);
+
+                if(!finalRepetition.contains(testChatRoom)){
+                    chatRoomName = testChatRoom;
+                    chattingList.push().setValue(chatRoomName);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        System.out.println("123123123: " + chatRoomName);
+        chatRoomName = testChatRoom;
+        System.out.println("123123123: " + chatRoomName);
+
         myRef = database.getReference(chatRoomName); // message받아오기
-        
         myRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
--- shareParking-AndroidStudio/app/src/main/java/com/example/parking/ui/chat/ChatFragment.java
+++ shareParking-AndroidStudio/app/src/main/java/com/example/parking/ui/chat/ChatFragment.java
@@ -1,15 +1,19 @@
 package com.example.parking.ui.chat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +24,7 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.parking.ChatActivity;
 import com.example.parking.ChatData;
+import com.example.parking.MainActivity;
 import com.example.parking.R;
 import com.example.parking.ui.setting.SettingViewModel;
 import com.google.firebase.database.ChildEventListener;
@@ -35,7 +40,8 @@ import java.util.List;
 public class ChatFragment extends Fragment {
     private DatabaseReference chattingList;
     private ChatViewModel chatViewModel;
-    private ArrayList<String> list2;
+    private ArrayList<String> list2, displayList;
+    public String reqUser;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -50,7 +56,15 @@ public class ChatFragment extends Fragment {
                 list2
         );
 
+        final SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("sFile", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
 
+        reqUser = sharedPreferences.getString("loginId","user");
+        System.out.println("*********************************************************************");
+        System.out.println(reqUser);
+        System.out.println("*********************************************************************");
+
+        displayList = new ArrayList<String>();
         list2 = new ArrayList<String>();// 채팅방 목록 표현해주기 위한 리스트
         FirebaseDatabase database = FirebaseDatabase.getInstance(); //데이터베이스 생성 및 선
         chattingList = database.getReference("chattingList"); //chattingList 값 가져오
@@ -60,19 +74,62 @@ public class ChatFragment extends Fragment {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 Log.d("ChattingList", "list : " + dataSnapshot.getValue());
+
+                displayList.clear();
+
                 list2.add(dataSnapshot.getValue().toString());
                 System.out.println("size:"+ list2.size());
-                System.out.println(s);
+                System.out.println("list2:"+ list2);
+
+                for(int i = 0; i < list2.size(); i ++) {
+                    String[] sep = list2.get(i).split("&");
+                    if(sep[0].equals(reqUser) || sep[1].equals(reqUser)){
+                        if(!sep[0].equals(reqUser)){
+                            displayList.add(sep[0]);
+                        }
+                        else if(!sep[1].equals(reqUser)){
+                            displayList.add(sep[1]);
+                        }
+                    }
+                }
+
                 System.out.println("--------------------------------채팅방리스트(in the void method)----------------------------------");
-//                for(int i = 0; i < list2.size(); i ++) {
-//                    System.out.println(list2.get(i));
-//                }
+
 
                 list_view.setAdapter(new ArrayAdapter<String>(
                         getContext(),
                         android.R.layout.simple_list_item_1,
-                        list2
+                        displayList
                 ));
+
+                list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        System.out.println(list2.size());
+                        System.out.println("윽액악악악: " + list2);
+
+                        for(int j = 0; j < list2.size(); j ++){
+                            String[] check = list2.get(j).split("&");
+                            if(check[0].equals(reqUser) && check[1].equals(displayList.get(i))){
+                                System.out.println("윽액악악악: " + list2.get(j));
+                                editor.putString("roomName", list2.get(j));
+                                editor.commit();
+                            }
+                            else if(check[1].equals(reqUser) && check[0].equals(displayList.get(i))){
+                                System.out.println("윽액악악악: " + list2.get(j));
+                                editor.putString("roomName", list2.get(j));
+                                editor.commit();
+                            }
+                        }
+
+                        String sendName = sharedPreferences.getString("roomName", "user");
+                        System.out.println("넌무엇이냐?: "+ sendName);
+
+//                        Intent intent = new Intent(getActivity(), ChatActivity.class);
+//                        startActivity(intent);
+                        Toast.makeText(getContext(), displayList.get(i), Toast.LENGTH_SHORT).show();
+                    }
+                });
 
                 System.out.println("-----------------------------------------------------------------------------------------------");
             }
@@ -104,27 +161,6 @@ public class ChatFragment extends Fragment {
             System.out.println(list2.get(i));
         }
         System.out.println("-----------------------------------------------------------------------------------------------");
-
-//        list_view.setAdapter(adapter);
-
-//        final TextView textView = root.findViewById(R.id.text_chat);
-//        Button button = root.findViewById(R.id.chatting_test);
-//
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getContext(), ChatActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        chatViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
-
-
 
         return root;
     }
