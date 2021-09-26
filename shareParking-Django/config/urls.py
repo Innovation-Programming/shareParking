84472ba7082b9b783fb7f2c6cfe98946258278de
takeny1998 <takeny1998@gmail.com>
2021-09-26 12:21:25 +0900

    merge static folders

--- shareParking-Django/config/urls.py
+++ shareParking-Django/config/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('map/', include('map.urls')),
     path('chat/', include('chat.urls')),
     # path('', views.index, name='index'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
